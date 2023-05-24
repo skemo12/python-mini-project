89587e530501027c533c74c17af3e3f01df3d5ef
skemo12 <alexdarius2012@yahoo.com>
2023-05-24 16:10:55 +0300

    Fixed Dice Rolling lint

--- Dice_Rolling_Stimulator/dice_stimulator.py
+++ Dice_Rolling_Stimulator/dice_stimulator.py
@@ -1,50 +1,52 @@
+""" Dice simulator """
+
 import random
 
 print("This is a dice stimulator")
-x = "y"
-while x == "y":
-    number = random.randint(1,6)
+X = "y"
+while X == "y":
+    NUMBER = random.randint(1, 6)
 
-    if number == 1:
+    if NUMBER == 1:
         print("===========")
         print("|         |")
         print("|    O    |")
-        print("|         |")
-        print("===========")
-
-    if number == 2:
-        print("===========")
-        print("|         |")
-        print("| O     O |")
         print("|         |")
         print("===========")
 
-    if number == 3:
+    if NUMBER == 2:
+        print("===========")
+        print("|         |")
+        print("| O     O |")
+        print("|         |")
+        print("===========")
+
+    if NUMBER == 3:
         print("===========")
         print("|    O    |")
         print("|    O    |")
         print("|    O    |")
         print("===========")
-        
-    if number == 4:
+
+    if NUMBER == 4:
         print("===========")
         print("| O     O |")
         print("|         |")
         print("| O     O |")
         print("===========")
-        
-    if number == 5:
+
+    if NUMBER == 5:
         print("===========")
         print("| O     O |")
         print("|    O    |")
         print("| O     O |")
         print("===========")
-        
-    if number == 6:
+
+    if NUMBER == 6:
         print("===========")
         print("| O     O |")
         print("| O     O |")
         print("| O     O |")
         print("===========")
-    
-    x =  input("Press y to roll again ")
+
+    X = input("Press y to roll again ")
